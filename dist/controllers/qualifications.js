b32fe304d0e732014ffca97ca0ee476a12a342b1
BernardoDiaz <bernardo17.bd@gmail.com>
2024-02-11 16:34:02 -0600

    GenerateQualification -v-f

--- dist/controllers/qualifications.js
+++ dist/controllers/qualifications.js
@@ -11,9 +11,48 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.verifyQualification = exports.GenerateQualification = void 0;
 const qualifications_1 = require("../models/qualifications");
+const level_1 = require("../models/level");
+const degree_1 = require("../models/degree");
+const subject_1 = require("../models/subject");
+const matricula_1 = require("../models/paymentsModels/matricula");
 const GenerateQualification = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const { IdDegree } = req.body;
+    const { IdDegree } = req.params;
+    const year = new Date().getFullYear();
     try {
+        // 1. Obtener el nivel del grado (número de periodos) desde la tabla "nivel"
+        const nivel = yield degree_1.degree.findOne({
+            where: { id: IdDegree },
+            attributes: [],
+            include: [{ model: level_1.level, attributes: ['periodsToEvaluate'] }],
+        });
+        const numeroDePeriodos = nivel === null || nivel === void 0 ? void 0 : nivel.level.periodsToEvaluate;
+        const alumnos = yield matricula_1.registration.findAll({
+            where: { id_degree: IdDegree, year: year },
+            attributes: ['id'],
+        });
+        // 2. Obtener las asignaturas vinculadas al grado
+        const asignaturas = yield subject_1.subject.findAll({
+            attributes: ['id'],
+            where: { id_degree: IdDegree },
+        });
+        // 3. Iterar por los periodos, asignaturas y alumnos
+        for (let periodo = 1; periodo <= numeroDePeriodos; periodo++) {
+            for (const asignatura of asignaturas) {
+                for (const alumno of alumnos) {
+                    // 4. Inserción de notas (ejemplo)
+                    const notaAlumno = {
+                        id_degree: IdDegree,
+                        id_subject: asignatura.id,
+                        id_registration: alumno.id,
+                        period: 'Periodo ' + periodo,
+                        rating: 0.00,
+                        year: year
+                    };
+                    yield qualifications_1.qualifications.create(notaAlumno);
+                }
+            }
+        }
+        return res.status(200).json({ msg: 'Notas generadas exitosamente' });
     }
     catch (error) {
         return res.status(500).json({
--- dist/routes/qualifications.js
+++ dist/routes/qualifications.js
@@ -7,6 +7,6 @@ const express_1 = require("express");
 const validate_token_1 = __importDefault(require("./UserRoute/validate-token"));
 const qualifications_1 = require("../controllers/qualifications");
 const router = (0, express_1.Router)();
-router.post('/', validate_token_1.default, qualifications_1.GenerateQualification);
+router.get('/:IdDegree', validate_token_1.default, qualifications_1.GenerateQualification);
 router.get('/vf/:IdDegree', validate_token_1.default, qualifications_1.verifyQualification);
 exports.default = router;

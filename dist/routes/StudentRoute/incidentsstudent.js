19026e7e9dfd467ee48498f563b22ff97a388125
BernardoDiaz <bernardo17.bd@gmail.com>
2024-03-31 19:35:55 -0600

    as idstudent

--- dist/routes/StudentRoute/incidentsstudent.js
+++ dist/routes/StudentRoute/incidentsstudent.js
@@ -8,7 +8,7 @@ const incidentsstudent_1 = require("../../controllers/StudentControllers/inciden
 const validate_token_1 = __importDefault(require("../UserRoute/validate-token"));
 const router = (0, express_1.Router)();
 router.get('/', validate_token_1.default, incidentsstudent_1.getIncidents);
-router.get('/:id', validate_token_1.default, incidentsstudent_1.getIncidentById);
+router.get('/:idStudent', validate_token_1.default, incidentsstudent_1.getIncidentById);
 router.get('/ed/:id', validate_token_1.default, incidentsstudent_1.getById);
 router.post('/', validate_token_1.default, incidentsstudent_1.newIncident);
 router.put('/:id', validate_token_1.default, incidentsstudent_1.updateIncident);
--- src/routes/StudentRoute/incidentsstudent.ts
+++ src/routes/StudentRoute/incidentsstudent.ts
@@ -5,11 +5,11 @@ import validateToken from '../UserRoute/validate-token';
 const router = Router();
 
 router.get('/', validateToken,getIncidents);
-router.get('/:id',validateToken,getIncidentById);
+router.get('/:idStudent',validateToken,getIncidentById);
 router.get('/ed/:id',validateToken,getById);
 router.post('/', validateToken,newIncident);
 router.put('/:id', validateToken,updateIncident);
 router.delete('/:id', validateToken,deleteIncident);
 
  
-export default router;  
+export default router;   

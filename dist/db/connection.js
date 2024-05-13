bd27a65ad4384a1ec461714eee9c240e52e68339
BernardoDiaz <bernardo17.bd@gmail.com>
2024-05-12 19:38:06 -0600

    conn

--- dist/db/connection.js
+++ dist/db/connection.js
@@ -1,12 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize('proyectlrd', 'root', 'root123456', {
-    host: 'localhost',
-    dialect: 'mysql'
-});
-// const sequelize = new Sequelize('proyectlrd','root','ed|6vB{4Yn}F5gA4',{
-//     host: '34.125.91.26', 
+// const sequelize = new Sequelize('proyectlrd','root','root123456',{
+//     host: 'localhost',
 //     dialect: 'mysql'
 // });
+const sequelize = new sequelize_1.Sequelize('proyectlrd', 'root', 'ed|6vB{4Yn}F5gA4', {
+    host: '34.125.91.26',
+    dialect: 'mysql'
+});
 exports.default = sequelize;
